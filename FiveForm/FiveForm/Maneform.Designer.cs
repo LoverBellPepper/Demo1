10f48483d05132c39a115388d336d98a9093e96f
LoverBellPepper <gmargo1515anime@mail.ru>
2023-03-21 13:23:36 +0900

    Add project1

---
+++ FiveForm/FiveForm/Maneform.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace FiveForm
+{
+    partial class FiveForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CloseButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // CloseButton
+            // 
+            this.CloseButton.Location = new System.Drawing.Point(99, 225);
+            this.CloseButton.Name = "CloseButton";
+            this.CloseButton.Size = new System.Drawing.Size(96, 40);
+            this.CloseButton.TabIndex = 0;
+            this.CloseButton.Text = "Close";
+            this.CloseButton.UseVisualStyleBackColor = true;
+            this.CloseButton.Click += new System.EventHandler(this.CloseButton_Click);
+            // 
+            // FiveForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.YellowGreen;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.CloseButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FiveForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "1";
+            this.Load += new System.EventHandler(this.FiveForm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button CloseButton;
+    }
+}
+
---
+++ FiveForm/FiveForm/Maneform.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+namespace FiveForm
+{
+    public partial class FiveForm : Form
+    {
+        public FiveForm()
+        {
+            InitializeComponent();
+        }
+
+        private void CloseButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void FiveForm_Load(object sender, EventArgs e)
+        {
+            System.Drawing.Drawing2D.GraphicsPath gp = new
+            System.Drawing.Drawing2D.GraphicsPath();
+            Point[] mp = new Point[4];
+            mp[0] = new Point(0, 0);
+            mp[1] = new Point(300, 0);
+            mp[2] = new Point(0, 300);
+            mp[3] = new Point(300, 300);
+
+            gp.AddPolygon(mp);
+            Region rg = new Region(gp);
+            this.Region = rg;
+
+            //mp[0] = new Point(0, 150);
+            //mp[1] = new Point(50, 300);
+            //mp[2] = new Point(420, 300);
+            //mp[3] = new Point(650, 150);
+            //mp[4] = new Point(400, 0);
+
+        }
+    }
+}
